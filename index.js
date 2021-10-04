ff97e7c677668fbe4bff59bc1ef1786b79684617
Fixito <thomas_3004@hotmail.fr>
2021-10-04 11:30:57 +0200

    fix of name

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import mongodb from './config.js';
 
 const { MONGODB } = mongodb;
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs,
