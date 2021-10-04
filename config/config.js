19d52a9ecb5b57099ba5f38f5ff8393144d48534
Fixito <thomas_3004@hotmail.fr>
2021-10-04 14:16:48 +0200

    fixed export

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 // Figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
   // We are in production - return the prod set of keys
-  module.exports = require('./prod.js');
+  export default require('./prod.js');
 } else {
   // We are in development - return the dev keys
-  module.exports = require('./dev.js');
+  export default require('./dev.js');
 }
