b423438b625bc966861ac6076323bd8ec0a73920
Fixito <thomas_3004@hotmail.fr>
2021-10-04 13:28:18 +0200

    fixed env var import

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 // keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
   // we are in production - return the prod set of keys
-  import './prod';
+  export default require('./prod');
 } else {
   // we are in development - return the dev keys!!
-  import './dev';
+  export default require('./dev');
 }
